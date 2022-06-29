b0a9812d15ca7fd94146ec99285a8241be3fff22
H13062 <bgh1506@gmail.com>
2022-06-29 14:53:17 -0500

    Create list of words in navbar

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -1,8 +1,51 @@
 import React from 'react'
+import {images} from '../../constants';
+import { GiHamburgerMenu } from 'react-icons/gi'; //must import from folder gi in react icons
+import {MdShoppingCart, MdOutlineRestaurantMenu} from 'react-icons/md'; //must import from folder md from react icons
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = React.useState(false);
+
   return (
-    <div>Navbar</div>
+    <nav className = "app__navbar">
+        {/*This part adding logo*/}
+        <div className='app__navbar-logo'> 
+          LOGO
+        </div>
+        {/*THis part contains words of navba*/}
+        <ul className = "app__navbar-links">
+            <li className = "p__opensans"><a href ="/home">Home</a></li>
+            <li className = "p__opensans"><a href ="/about">About</a></li>
+            <li className = "p__opensans"><a href ="/menu">Menu</a></li>
+            <li className = "p__opensans"><a href ="/contacts">Contacts</a></li>
+            <li className = "p__opensans"><a href ="/comment">Comment</a></li>
+        </ul>
+        <div className = 'app__navbar-access'>
+            <a href ="/login" className ="p__opensans">Login / Register</a>
+            <div/>
+            <a href ="/order" className ="p__opensans">Order</a>
+            <div/>
+            <MdShoppingCart />
+        </div>
+        <div className = "app__navbar-mobile">
+            <GiHamburgerMenu color= '#000000' fontSize = {27} onClick= {() => setToggleMenu(true)} />
+        </div>
+        {toggleMenu && (
+                <div className='app__navbar-smallscreen_overlay '>
+                      <MdOutlineRestaurantMenu fontSize={27} className ="overlay__close" onClick={() => setToggleMenu(false)}/>
+                      <ul className = "app__navbar-smallscreen-links">
+                        <li className = "p__opensans"><a href ="/home">Home</a></li>
+                        <li className = "p__opensans"><a href ="/about">About</a></li>
+                        <li className = "p__opensans"><a href ="/menu">Menu</a></li>
+                        <li className = "p__opensans"><a href ="/contacts">Contacts</a></li>
+                        <li className = "p__opensans"><a href ="/comment">Comment</a></li>
+                        <li className = "p__opensans"><a href ="/login">Login/Register</a></li>
+                      </ul>
+                </div>
+              )
+        }
+    </nav>
+        
   )
 }
 
