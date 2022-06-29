08eecc8d9dda2360871de149d046f7a16b614439
H13062 <bgh1506@gmail.com>
2022-06-28 21:31:29 -0500

    adding data.js

---
+++ src/constants/data.js
@@ -0,0 +1,42 @@
+import images from "./images";
+
+const food =[
+    {
+        imgUrl: images.ct,
+        title: 'Com suon',
+        price: '12$',
+        tags: 'per serving',
+    },
+    {
+        title: 'Com suon',
+        price: '12$',
+        tags: 'per serving',
+    },
+    {
+        title: 'Com suon',
+        price: '12$',
+        tags: 'per serving',
+    },
+    {
+        title: 'Com suon',
+        price: '12$',
+        tags: 'per serving',
+    },
+    {
+        title: 'Com suon',
+        price: '12$',
+        tags: 'per serving',
+    },
+    {
+        title: 'Com suon',
+        price: '12$',
+        tags: 'per serving',
+    },
+    {
+        title: 'Com suon',
+        price: '12$',
+        tags: 'per serving',
+    },
+];
+
+export default {food};
