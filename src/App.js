6f151e4d6834d3ecc9d4914e52cb82345545e46e
NPNG22 <nguyendinhcaodat@gmail.com>
2022-06-29 12:33:46 -0600

    add Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import { Register, AboutUs } from "./pages"
+import {Navbar} from "./component"
 
 function App() {
   return (
     <>
+      <Navbar/>
        <Register />
       {/* <AboutUs /> */}
     </>
