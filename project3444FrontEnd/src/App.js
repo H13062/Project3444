492ba5447bae819576f13645b4186519b9779a70
Minh Than <hoangminh117@gmail.com>
2022-07-28 21:42:04 -0500

    made changes to the routing (wrong branch)

--- project3444FrontEnd/src/App.js
+++ project3444FrontEnd/src/App.js
@@ -3,19 +3,20 @@ import "./App.css";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import { Register, AboutUs, Comment, Home, Menu, Contact } from "./pages";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import { Navbar } from "./component";
 function App() {
   return (
     <>
       <Router>
+        <Navbar />
         <Routes>
-          <Route path="/project3444" element={<Home/>}/>
-          <Route path="/home" element={<Home/>}/>
-          <Route path="/login" element={<Register />}/>
-          <Route path="/about" element={<AboutUs />}/>
-          <Route path="/contacts" element={<Contact />}/>
-          <Route path="/comment" element={<Comment />}/>
-          <Route path="/menu" element={<Menu />}/>
+          <Route path="/project3444" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Register />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/contacts" element={<Contact />} />
+          <Route path="/comment" element={<Comment />} />
+          <Route path="/menu" element={<Menu />} />
         </Routes>
       </Router>
       {/* <Register /> */}
